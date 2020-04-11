3cd9651ae8dffff5f6bd8bad4f0c75df92d5fea9
Tejas1698 <55478791+Tejas1698@users.noreply.github.com>
2020-04-11 15:05:16 +0530

    Add files via upload

---
+++ Activities.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PARADISE ROYALE</title>
+	<link rel="stylesheet" type="text/css" href="club.css">
+	<script type="text/javascript">
+		var image1=new Image();
+		image1.src="download1.jpg";
+		var image2=new Image();
+		image2.src="download2.jpg";
+		var image3=new Image();
+		image3.src="download3.jpg";
+		var image4=new Image();
+		image4.src="download4.jpg";
+		var image5=new Image();
+		image5.src="download5.jpg";
+		var image6=new Image();
+		image6.src="download6.jpg";
+		var image7=new Image();
+		image7.src="download7.jpg";
+		var image8=new Image();
+		image8.src="download8.jpg";
+		var image9=new Image();
+		image9.src="download9.jpg";
+		var image10=new Image();
+		image10.src="download10.jpg";
+		var image11=new Image();
+		image11.src="download11.jpg";
+		var image12=new Image();
+		image12.src="download12.jpg";
+		var image13=new Image();
+		image13.src="download13.jpg";
+		var image14=new Image();
+		image14.src="download14.jpg";
+		var image15=new Image();
+		image15.src="download15.jpg";
+		var image16=new Image();
+		image16.src="download16.jpg";
+		var image17=new Image();
+		image17.src="download17.jpg";
+		var image18=new Image();
+		image18.src="download18.jpg";
+		var image19=new Image();
+		image19.src="download19.jpg";
+		var image20=new Image();
+		image20.src="download20.jpg";
+	</script>
+</head>
+<body id="first">
+	<section class="div1">
+	<i><h1 class="name" align="center" style="font-weight: bold; font-size: 70px; color: white; top: 10px;">PARADISE ROYALE</h1></i>
+		<a href="About.html#about"><input type="button" style="margin-left: 1290px" value="About" onclick="#" class="non1"></a>
+		<a href="index.php#home"><input type="button" style="margin-right: 1299px" value="Home" onclick="#" class="non3"></a>
+		
+	</section>
+	<header>
+		<nav>
+			<ul>
+			<li><a id="but1" style="color: black">Holiday Package</a>
+				<ul>
+					<li class="hide1" title="View Details"><a style="color: black">Foosball</a></li>
+					<li class="hide1" title="View Details"><a style="color: black">Pool</a></li>
+					<li class="hide1" title="View Details"><a style="color: black">Snooker</a></li>
+					<li class="hide1" title="View Details"><a style="color: black">Table Tennis</a></li>
+				</ul>
+			</li>
+			<li><a id="but2" style="color: black">Fitness Sport Package</a>
+				<ul>
+					<li class="hide2" title="View Details"><a style="color: black">Box Cricket</a></li>
+					<li class="hide2" title="View Details"><a style="color: black">Trucking</a></li>
+					<li class="hide2" title="View Details"><a style="color: black">Futnet</a></li>
+					<li class="hide2" title="View Details"><a style="color: black">Rugby</a></li>
+					<li class="hide2" title="View Details"><a style="color: black">Ultimate Frisbee</a></li>
+					<li class="hide2" title="View Details"><a style="color: black">Yoga</a></li>
+				</ul>
+			</li>
+			<li><a id="but3" style="color: black">Sports</a>
+				<ul>
+					<li class="hide3" title="View Details"><a style="color: black">Badminton</a></li>
+					<li class="hide3" title="View Details"><a style="color: black">Squash</a></li>
+					<li class="hide3" title="View Details"><a style="color: black">Table Tennis</a></li>
+				</ul>
+			</li>
+			<li><a id="but4" style="color: black">Honeymoon Packages</a>
+				<ul>
+					<li class="hide4" title="View Details"><a style="color: black">Candlelight dinner</a></li>
+					<li class="hide4" title="View Details"><a style="color: black">Beach hopping</a></li>
+				</ul>
+			</li>
+			<li><a id="but5" style="color: black">Sightseeings</a>
+				<ul>
+					<li class="hide5" title="View Details"><a style="color: black">Wild Safari</a></li>
+					<li class="hide5" title="View Details"><a style="color: black">Museum</a></li>
+					<li class="hide5" title="View Details"><a style="color: black">Lake view</a></li>
+				</ul>
+			</li>
+			</ul>
+		</nav>
+			<img class="imgid1" src="download1.jpg" alt="Couldnt display" name="slide1" width="25%" height="250">
+			<img class="imgid2" src="download9.jpg" alt="Couldnt display" name="slide2" width="20%" height="250">
+			<img class="imgid3" src="download21.jpg" alt="Couldnt display" name="slide3" width="19%" height="250">
+			<img class="imgid4" src="download26.JPG" alt="Couldnt display" name="slide4" width="19%" height="250">
+			<img class="imgid5" src="download32.jpg" alt="Couldnt display" name="slide5" width="20%" height="250">
+		<script type="text/javascript">
+			var step1=1;
+			function slideit1()
+			{
+				document.images.slide1.src=eval("image"+step1+".src");
+				if(step1<4)
+					step1++;
+				else
+					step1=1;
+				setTimeout("slideit1()",2000);
+			}
+			slideit1();
+
+			var step2=5;
+			function slideit2()
+			{
+				document.images.slide2.src=eval("image"+step2+".src");
+				if(step2<8)
+					step2++;
+				else
+					step2=5;
+				setTimeout("slideit2()",2000);
+			}
+			slideit2();
+
+			var step3=9;
+			function slideit3()
+			{
+				document.images.slide3.src=eval("image"+step3+".src");
+				if(step3<12)
+					step3++;
+				else
+					step3=9;
+				setTimeout("slideit3()",2000);
+			}
+			slideit3();
+
+			var step4=13;
+			function slideit4()
+			{
+				document.images.slide4.src=eval("image"+step4+".src");
+				if(step4<16)
+					step4++;
+				else
+					step4=13;
+				setTimeout("slideit4()",2000);
+			}
+			slideit4();
+
+			var step5=17;
+			function slideit5()
+			{
+				document.images.slide5.src=eval("image"+step5+".src");
+				if(step5<20)
+					step5++;
+				else
+					step5=17;
+				setTimeout("slideit5()",2000);
+			}
+			slideit5();
+		</script>
+	</header>
+</body>
+</html>
